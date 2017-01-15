90ffdd6468accaa5af0d325cda6235962d53f382
Thanh C. Tran <trancong@rhrk.uni-kl.de>
2017-01-15 17:16:19 +0100

    Implement plotData function
    # Explain what has been changed
    
    # Explain why this change is being made
    
    # Provide links to any relevant tickets, articles or other resources

--- machine-learning-ex1/ex1/plotData.m
+++ machine-learning-ex1/ex1/plotData.m
@@ -15,8 +15,10 @@ figure; % open a new figure window
 % Hint: You can use the 'rx' option with plot to have the markers
 %       appear as red crosses. Furthermore, you can make the
 %       markers larger by using plot(..., 'rx', 'MarkerSize', 10);
-
-
+plot(x, y, 'rx', 'MarkerSize', 10);
+xlabel('City Populations in 10,000s');
+ylabel('Profits in $10,000s');
+title('Profits vs. City Populations');
 
 
 
