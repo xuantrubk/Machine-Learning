dc65a69c76cb926e90b919bef68eb25a1df465fb
Thanh C. Tran <trancong@rhrk.uni-kl.de>
2017-01-16 01:29:14 +0100

    Add new house price calculation for both methods (gradient descent & normal equation)
    # Explain what has been changed
    
    # Explain why this change is being made
    
    # Provide links to any relevant tickets, articles or other resources

--- machine-learning-ex1/ex1/ex1_multi.m
+++ machine-learning-ex1/ex1/ex1_multi.m
@@ -104,7 +104,9 @@ fprintf('\n');
 % ====================== YOUR CODE HERE ======================
 % Recall that the first column of X is all-ones. Thus, it does
 % not need to be normalized.
-price = 0; % You should change this
+new_house = ([1650 3] - mu) ./ sigma;
+new_house = [1 new_house];
+price = new_house * theta; % You should change this
 
 
 % ============================================================
@@ -149,7 +151,8 @@ fprintf('\n');
 
 % Estimate the price of a 1650 sq-ft, 3 br house
 % ====================== YOUR CODE HERE ======================
-price = 0; % You should change this
+new_house = [1; 1650; 3];
+price = theta' * new_house; % You should change this
 
 
 % ============================================================
